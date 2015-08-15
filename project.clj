3eb4124ab8f2c00c6247a0e50971edabac4201bf
dwiechert <dan.wiechert@gmail.com>
2015-08-14 20:35:25 -0500

    Got project working in Eclipse with clockwise plug-in.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject joy-of-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Working through examples in 'The Joyy of Clojure'"
+  :url "https://github.com/DWiechert/joy-of-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
