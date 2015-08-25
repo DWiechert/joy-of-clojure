b035695d5e84a48bbe0f7ce1cdf29b7884e607d3
dwiechert <dan.wiechert@gmail.com>
2015-08-25 17:39:11 -0500

    Done with chapter 3 section 3 listing 1.

---
+++ src/chapter3/section3/listing1.clj
@@ -0,0 +1,13 @@
+(ns chapter3.section3.listing1)
+
+(def guys-whole-name ["Guy" "Lewis" "Steele"])
+
+; This is more confusing that just manipulating the vector itself
+(str (nth guys-whole-name 2) ", "
+     (nth guys-whole-name 0) " "
+     (nth guys-whole-name 1))
+
+; Here we are destructuring the vector of the whole name into local vars
+; This type of positional destructuring only works on sorted items (not Sets or Maps)
+(let [[f-name m-name l-name] guys-whole-name]
+  (str l-name ", " f-name " " m-name))
