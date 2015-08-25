91a9686ee23660e59f8a3351305ef2a084001f77
dwiechert <dan.wiechert@gmail.com>
2015-08-25 17:39:36 -0500

    Done with chapter 3 section 3 listing 2.

---
+++ src/chapter3/section3/listing2.clj
@@ -0,0 +1,14 @@
+(ns chapter3.section3.listing2)
+
+; We can use & to denote any extra parameters into another sequence
+(let [[a b c & more] (range 10)]
+  (println "a b c are: " a b c)
+  (println "more is: " more))
+
+; We can also destructure into individual vars and keep the whole vector
+; We use :as after the & var
+(let [range-vec (vec (range 10))
+      [a b c & more :as all] range-vec]
+      (println "a b c are: " a b c)
+      (println "more is: " more)
+      (println "all is: " all))
